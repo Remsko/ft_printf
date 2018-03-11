f207bf0e284b62d21c6b41a29baac8dc09ac1101
Remi PINOIT <rpinoit@e2r10p12.42.fr>
2018-03-11 13:20:39 +0100

    preparing types

--- incs/ft_printf.h
+++ incs/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/11 11:27:23 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/03/11 11:41:16 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/03/11 13:20:09 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 
 # include "libft.h"
 # include "types.h"
+# include "stdarg.h"
 
 typedef struct	s_env
 {
-	int			tmp;
+	t_arg_def	*lst_args_def;
+	va_list		arg;
+	int			ret;
 }				t_env;
 
 #endif
--- incs/types.h
+++ incs/types.h
@@ -6,7 +6,7 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/11 11:34:53 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/03/11 11:42:05 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/03/11 13:20:07 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,34 @@
 # define TYPES_H
 
 # include "ft_printf.h"
+
+typedef enum	s_conv_enum
+{
+	s,
+	S,
+	p,
+	d,
+	D,
+	i,
+	o,
+	O,
+	u,
+	U,
+	x,
+	X,
+	c,
+	C
+}				t_conv_enum;
+
+typedef struct		s_arg_def
+{
+	t_conv_enum		letter; //conversion
+	void			*auth;	//argument en question
+	//flags #0-+ et ' '
+	//taille minimum
+	//precision
+	//flags hh h l ll j z
+	struct s_arg	next;
+}					t_arg_def;
 
 #endif
